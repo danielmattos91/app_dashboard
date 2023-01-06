6f9ae72dedeb9b873b24476c6392943fa8b02146
Daniel <mattosfacul@gmail.com>
2023-01-06 19:18:00 -0300

    Criado requisão com bd e classes para interação com app

---
+++ app.php
@@ -0,0 +1,117 @@
+<?php
+
+//classe dashboard
+class Dashboard {
+
+	public $data_inicio;
+	public $data_fim;
+	public $numeroVendas;
+	public $totalVendas;
+
+	public function __get($atributo) {
+		return $this->$atributo;
+	}
+
+	public function __set($atributo, $valor) {
+		$this->$atributo = $valor;
+		return $this;
+	}
+}
+
+//classe de conexão bd
+class Conexao {
+	private $host = 'localhost';
+	private $dbname = 'dashboard';
+	private $user = 'root';
+	private $pass = '';
+
+	public function conectar() {
+		try {
+
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"
+			);
+
+			//
+			$conexao->exec('set charset set utf8');
+
+			return $conexao;
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessege().'</p>';
+		}
+	}
+}
+
+//classe (model)
+class Bd {
+	private $conexao;
+	private $dashboard;
+
+	public function __construct(Conexao $conexao, Dashboard $dashboard) {
+		$this->conexao = $conexao->conectar();
+		$this->dashboard = $dashboard;
+	}
+
+	public function getNumeroVendas() {
+		$query = '
+			select 
+				count(*) as numero_vendas 
+			from 
+				tb_vendas 
+			where 
+				data_venda between :data_inicio and :data_fim';
+
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+		$stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+	}
+
+	public function getTotalVendas() {
+		$query = '
+			select 
+				SUM(total) as total_vendas 
+			from 
+				tb_vendas 
+			where 
+				data_venda between :data_inicio and :data_fim';
+
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+		$stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+	}
+}
+
+
+//lógica do script
+$dashboard = new Dashboard();
+
+$conexao = new Conexao();
+
+$competencia = explode('-', $_GET['competencia']);
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+$dashboard->__set('data_inicio', $ano.'-'.$mes.'-01');
+$dashboard->__set('data_fim', $ano.'-'.$mes.'-'.$dias_do_mes);
+
+
+$bd = new Bd($conexao, $dashboard);
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+$dashboard->__set('totalVendas', $bd->getTotalVendas());
+echo json_encode($dashboard);
+
+
+
+?>
--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 $(document).ready(() => {
 	
+	$('#documentacao').on('click', () => {
+		//$('#pagina').load('documentacao.html')
+
+		/*
+		$.get('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
+
+	$('#suporte').on('click', () => {
+		//$('#pagina').load('suporte.html')
+
+		/*
+		$.get('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
+
+
+	//ajax
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+		
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded
+			dataType: 'json',
+			success: dados => { 
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			},
+			error: erro => { console.log(erro) }
+		})
+
+		//método, url, dados, sucesso, erro
+	})
 })
